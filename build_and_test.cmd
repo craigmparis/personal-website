9af798a3021d2434be942d202e4f9155524f7c0e
craig paris <pariscraigm@gmail.com>
2022-10-03 19:54:31 -0700

    e - Add some verbosity to build_and_test.cmd

--- build_and_test.cmd
+++ build_and_test.cmd
@@ -1,3 +1,11 @@
-yarn install
-yarn build
-yarn test
+yarn install        || GOTO: exit
+yarn build          || GOTO: exit
+yarn test           || GOTO: exit
+
+ECHO "Build and test successful"
+EXIT /B 0
+
+:exit
+
+ECHO "Error running build_and_test.cmd"
+EXIT /B 1
