f1f71c0bb2a9f5360135ddc8329c265a20b8df9c
Craig Paris <cp@pocketpropertiesapp.com>
2022-11-12 20:31:50 -0800

    a - editorconfig

---
+++ src/components/TopNavBar/topNavBar.tsx
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+const Container = styled.div<{ name?: string }>``;
+
+export default function TopNavBar() {
+	return <Container name="top-navbar" role="navigation" aria-label="Main" />;
+}
---
+++ src/components/topNavBar.test.tsx
@@ -0,0 +1,6 @@
+import { render } from "@testing-library/react";
+import TopNavBar from "./TopNavBar/topNavBar";
+
+test("renders without crashing", () => {
+	render(<TopNavBar />);
+});
