271299b74a8280af4cee9fece03816b3283bc73b
pariscraigm@gmail.com <Craig Paris>
2022-11-13 19:14:18 -0800

    F** Add minimal terraform module with aws provider

---
+++ infrastructure/main.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "4.39.0"
+    }
+  }
+
+  required_version = "1.3.4"
+}
+
+provider "aws" {
+  region  = "us-east-1"
+}
