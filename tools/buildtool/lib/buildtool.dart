1a48ba80404dcdc130dd3be5708313b7905d4831
pariscraigm@gmail.com <Craig Paris>
2022-11-14 20:58:14 -0800

    F** Add ability to extract a zip file

--- tools/buildtool/lib/buildtool.dart
+++ tools/buildtool/lib/buildtool.dart
@@ -1,16 +1,27 @@
+import 'package:archive/archive_io.dart';
 import 'package:path/path.dart' as p;
 
 import 'dart:io';
 
-Future<File> downloadFile(final Uri url, final Directory outputDirectory) async {
+Future<File> downloadFile(
+    final Uri url, final Directory outputDirectory) async {
   var output = File(p.join(outputDirectory.path, url.pathSegments.last));
   var client = HttpClient();
   try {
     HttpClientRequest request = await client.getUrl(url);
     HttpClientResponse response = await request.close();
-    response.pipe(output.openWrite());
+    await response.pipe(output.openWrite());
   } finally {
     client.close();
   }
+  return output;
+}
+
+Directory extractArchive(File archive) {
+  final output = Directory(p.join(
+    p.dirname(archive.path),
+    p.basenameWithoutExtension(archive.path)
+  ));
+  extractFileToDisk(archive.path, output.path);
   return output;
 }
--- tools/buildtool/test/buildtool_test.dart
+++ tools/buildtool/test/buildtool_test.dart
@@ -1,17 +1,25 @@
+import 'package:archive/archive_io.dart';
 import 'package:buildtool/buildtool.dart';
 import 'package:test/test.dart';
 import 'dart:io';
 
 void main() {
+  final dartSDKLocation = Uri.parse(
+      "https://storage.googleapis.com/dart-archive/channels/stable/release/2.18.4/sdk/dartsdk-linux-arm64-release.zip");
+  final tempDir = Directory.systemTemp.createTempSync();
+  late final File archive;
+  late final Directory inflatedArchive;
+
+  setUpAll((() async {
+    archive = await downloadFile(dartSDKLocation, tempDir);
+    inflatedArchive = extractArchive(archive);
+  }));
+
   test('Can download the Dart SDK', () async {
-    // Given a url and an output path
-    final dartSDKLocation = Uri.parse("https://storage.googleapis.com/dart-archive/channels/stable/release/2.18.4/sdk/dartsdk-linux-arm64-release.zip");
-    final tempDir = Directory.systemTemp.createTempSync();
-
-    // When downloadArchive is called
-    final File archive = await downloadFile(dartSDKLocation, tempDir);
-
-    // Then the archive is downloaded successfully
     expect(await archive.exists(), true);
+  });
+
+  test('Can extract a zip file', () async {
+    expect(await inflatedArchive.exists(), true);
   });
 }
